5a894aa2cc511ce7dcbf54d608ad27f2fc106dbd
ragil800 <ragilcreator800@gmail.com>
2026-04-20 09:35:07 +0700

    Add files via upload

---
+++ JavaApplication1/build/classes/appconstructor.java
@@ -0,0 +1,9 @@
+
+public class appconstructor {
+    public static void main(String[] args) {
+    
+        constructor satu = new constructor();
+        constructor dua = new constructor("ragil");
+        constructor tiga = new constructor(17);
+    }
+}
---
+++ JavaApplication1/build/classes/constructor.java
@@ -0,0 +1,17 @@
+public class constructor {
+    
+    String nama;
+    int umur;
+    
+    constructor() {
+        System.out.println("nama peserta");
+    }
+    
+    constructor(String nama) {
+        System.out.println("nama anda : " + nama);
+    }
+    
+    constructor(int umur) {
+        System.out.println("umur anda : " + umur);
+    }
+}
